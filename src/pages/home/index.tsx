b0dcb10ab7eacbd8c796cf9b563b42ee8cbccb0d
Adwaith Rao <adwaith@webtrigon.com>
2022-06-18 22:00:16 +0530

    Add a text section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,43 @@
 import styles from "./index.module.scss";
 
 export function Home() {
-  return <div className={styles.home}></div>;
+  return <div className={styles.home}>
+    <section className={styles.textSection}>
+      <div className={styles.textWrapper}>
+        <h2>Section heading</h2>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium officia impedit voluptatibus commodi placeat nulla, expedita ea quisquam velit aliquam ipsum quae modi sed tempore provident similique error obcaecati. Quam!
+        Recusandae iste odit saepe eveniet optio eos incidunt magni, dolores nemo assumenda nesciunt iusto, voluptatum sequi explicabo obcaecati ad dolorem quod! Omnis iure assumenda incidunt! Suscipit hic dolores ipsa laudantium!
+        Porro, quidem excepturi, voluptatem maxime et consequuntur similique omnis minima praesentium sapiente corporis fugiat ipsam odit! Voluptates, velit! Eligendi voluptatem, consequuntur quos vitae officia veritatis autem praesentium eveniet nihil debitis.
+        Fuga, dolorum doloribus necessitatibus nostrum architecto soluta deleniti veniam. Cupiditate, veritatis debitis maxime illum dignissimos esse ea? Voluptates sed ea laudantium sint placeat illo. Laboriosam quasi sint illo inventore nihil.
+        Molestiae aperiam harum exercitationem quia iste praesentium, repellendus perspiciatis nihil. Sequi nam deserunt tempore, quod cupiditate fugiat quibusdam sapiente quos expedita ad pariatur voluptatum eveniet beatae culpa laboriosam porro possimus.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt vero voluptatibus alias facere, magnam blanditiis eligendi adipisci minus delectus reprehenderit quae et repudiandae dicta omnis excepturi, aspernatur dignissimos neque! Quae.
+        Amet rem deserunt repellendus quam cumque incidunt suscipit quod officiis facilis est quo ratione hic fugiat id dolorem corrupti veritatis, mollitia temporibus modi voluptatum deleniti! Labore aut debitis illo deserunt.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolorum nisi consequuntur? Debitis quis quia numquam, fugit illum assumenda nesciunt repellat dignissimos eaque ipsum mollitia, consequuntur voluptatem laudantium asperiores alias?
+        Iure rem impedit nobis est nisi, fuga eius, deserunt vero delectus voluptatem blanditiis! Autem, consectetur totam odio accusantium enim, dolorem officiis sunt libero ab est optio labore ea, eius vitae.
+        Non magnam distinctio aliquam dolores ipsa id, soluta odit vero eveniet ipsum! Quam voluptatibus ea, natus nobis expedita pariatur suscipit nulla, sit omnis nam laborum asperiores tenetur numquam eaque dolorem?</p>
+      </div>
+    </section>
+    
+    <section className={styles.textSection}>
+      <div className={styles.textWrapper}>
+        <h2>Section heading</h2>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium officia impedit voluptatibus commodi placeat nulla, expedita ea quisquam velit aliquam ipsum quae modi sed tempore provident similique error obcaecati. Quam!
+        Recusandae iste odit saepe eveniet optio eos incidunt magni, dolores nemo assumenda nesciunt iusto, voluptatum sequi explicabo obcaecati ad dolorem quod! Omnis iure assumenda incidunt! Suscipit hic dolores ipsa laudantium!
+        Porro, quidem excepturi, voluptatem maxime et consequuntur similique omnis minima praesentium sapiente corporis fugiat ipsam odit! Voluptates, velit! Eligendi voluptatem, consequuntur quos vitae officia veritatis autem praesentium eveniet nihil debitis.
+        Fuga, dolorum doloribus necessitatibus nostrum architecto soluta deleniti veniam. Cupiditate, veritatis debitis maxime illum dignissimos esse ea? Voluptates sed ea laudantium sint placeat illo. Laboriosam quasi sint illo inventore nihil.
+        Molestiae aperiam harum exercitationem quia iste praesentium, repellendus perspiciatis nihil. Sequi nam deserunt tempore, quod cupiditate fugiat quibusdam sapiente quos expedita ad pariatur voluptatum eveniet beatae culpa laboriosam porro possimus.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt vero voluptatibus alias facere, magnam blanditiis eligendi adipisci minus delectus reprehenderit quae et repudiandae dicta omnis excepturi, aspernatur dignissimos neque! Quae.
+        Amet rem deserunt repellendus quam cumque incidunt suscipit quod officiis facilis est quo ratione hic fugiat id dolorem corrupti veritatis, mollitia temporibus modi voluptatum deleniti! Labore aut debitis illo deserunt.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolorum nisi consequuntur? Debitis quis quia numquam, fugit illum assumenda nesciunt repellat dignissimos eaque ipsum mollitia, consequuntur voluptatem laudantium asperiores alias?
+        Iure rem impedit nobis est nisi, fuga eius, deserunt vero delectus voluptatem blanditiis! Autem, consectetur totam odio accusantium enim, dolorem officiis sunt libero ab est optio labore ea, eius vitae.
+        Non magnam distinctio aliquam dolores ipsa id, soluta odit vero eveniet ipsum! Quam voluptatibus ea, natus nobis expedita pariatur suscipit nulla, sit omnis nam laborum asperiores tenetur numquam eaque dolorem?</p>
+      </div>
+    </section>
+  </div>;
 }
