b9b86f3cddc8c7478e2b397fc16357a37c118fc5
Adwaith Rao <adwaith@webtrigon.com>
2022-06-18 21:48:12 +0530

    Set up basic gallery

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,5 +1,36 @@
+import ImageGallery from 'react-image-gallery';
 import styles from "./index.module.scss";
 
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    originalTitle: 'Tooltip for image',
+    description: 'Basic caption for image',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    originalTitle: 'Tooltip for image',
+    description: 'Basic caption for image',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    originalTitle: 'Tooltip for image',
+    description: 'Basic caption for image',
+  },
+];
+
 export function Gallery() {
-  return <div className={styles.gallery}></div>;
+  return <div className={styles.gallery}>
+    <h1>Gallery</h1>
+    <div className={styles.photosContainer}>
+      <ImageGallery 
+        items={images} 
+        showNav={false}
+        showPlayButton={false}
+      />
+    </div>
+  </div>;
 }
